b416c4477e649cd4037cb5e903f7a282e4e8a861
dmitrysbn <dmitry.sbn@gmail.com>
2018-01-30 14:08:17 -0500

    Exercise 5: greet_backwards

---
+++ exercise5.rb
@@ -0,0 +1,8 @@
+def greet_backwards(my_name)
+  return "Hello, #{my_name.reverse + my_name.reverse}! Welcome home."
+end
+
+greet_backwards("Bob")
+greet_backwards("Shirly")
+greet_backwards("Sue")
+greet_backwards("Andy")
