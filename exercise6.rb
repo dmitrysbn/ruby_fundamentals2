55c6b2f89975aea58abad074e1475d85941c4cd3
dmitrysbn <dmitry.sbn@gmail.com>
2018-01-30 14:26:57 -0500

    Exercise 6: Fahrenheit (updated)

--- exercise6.rb
+++ exercise6.rb
@@ -1,8 +1,9 @@
 puts "Please enter temperature in Fahrenheit"
 input_fahrenheit = gets.chomp.to_i
 
-def fahrenheit_to_celsius(fahrenheit)
-  return (fahrenheit - 32) * 5 / 9
+def fahrenheit_to_celsius(input_fahrenheit)
+  return (input_fahrenheit - 32) * 5 / 9
 end
 
+puts fahrenheit_to_celsius(40)
 puts "#{input_fahrenheit} Fahrenheit is #{fahrenheit_to_celsius(input_fahrenheit)} Celsius."
